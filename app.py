dc297ef72f1543fc54edc0acb774967ef34bf723
Divijeshvarma.a <divijeshvarma.a@pidatacenters.com>
2025-10-22 14:30:08 +0530

    flask2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! Welcome to your Flask app."
+    return "Hello, World! Welcome to Divi app."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
