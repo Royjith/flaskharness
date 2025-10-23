cad174bb358146ba16b5ecafcb855b13298b9513
Divijeshvarma.a <divijeshvarma.a@pidatacenters.com>
2025-10-23 09:08:51 +0530

    flask5

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! Welcome to varma app."
+    return "Hello, World! Welcome to varma"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
